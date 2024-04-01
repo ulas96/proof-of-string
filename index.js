8e8b7fd5b0791d550d5b45034c01fcae073f4ea2
ulas96 <y.ulas.yildiz@gmail.com>
2024-04-01 09:27:41 +0300

    update v1.0.2
    Function returns corresponding hash and string values to be used easier in the flow of code

--- index.js
+++ index.js
@@ -1,29 +1,17 @@
 const axios = require("axios");
 
-const readHashBond = (hash) => {
+const readHashBond = async (hash) => {
     if(hash.length === 64) {
-        axios.post(`https://q3q0eo8qde.execute-api.eu-central-1.amazonaws.com/default/post-read?hash=${hash}`)
-        .then(response => {
-        if(response.data) {
-            console.log('Corresponding string is', response.data.string.S)
-        } else {
-            console.log('Hash string combination not bonded')
-        }
-        }).catch(error => {
-        console.error(error);
-        });
+        const string = await axios.post(`https://q3q0eo8qde.execute-api.eu-central-1.amazonaws.com/default/post-read?hash=${hash}`)
+        return string.data.string.S;
     } else  {
         console.log('Hash must be in hexedecimal format')
     }
 }
 
 const bondHash = async (string) => {
-        return axios.post(`https://ms98jjxn9d.execute-api.eu-central-1.amazonaws.com/default/post-insert?string=${string}`)
-        .then(response => {
-            console.log(response.data);
-        }).catch(error => {
-        console.error(error);
-        });
+        const hash = await axios.post(`https://ms98jjxn9d.execute-api.eu-central-1.amazonaws.com/default/post-insert?string=${string}`)
+        return hash.data;
     }
 
 
